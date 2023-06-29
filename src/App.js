64b00eaa2c21f0175c40951890ab05deffe85762
unknown <jadsoonbol@gmail.com>
2023-06-28 21:27:47 -0300

    começando autenticação

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login } from "./pages/login";
 import { Home } from "./pages/home";
+import Routes from './routes';
+import history from './history';
+
+import { AuthProvider } from './Context/AuthContext';
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes history={history}>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
